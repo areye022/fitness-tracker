669e917f477007a526ef2eaf5731f46df57fd70a
areye022 <areye022@gmail.com>
2020-12-01 17:29:30 -0800

    added server.js boilerplate info, next I will be creating a model and routes

---
+++ server.js
@@ -0,0 +1,27 @@
+// Add code to userModel.js to complete the model
+
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// const User = require("./userModel.js");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true });
+
+// Routes
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
